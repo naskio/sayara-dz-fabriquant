362880621af480a7b878f4944a56b7b4284a30b0
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2019-09-16 03:16:41 +0100

    Updates

--- precache-manifest.df693f98b87160af314744afee766fa5.js
+++ precache-manifest.df693f98b87160af314744afee766fa5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2c3622818b59a5aadf14cfc1912637ea",
+    "revision": "9a15e379f1a043b887ce3b390fa552fe",
     "url": "/sayara-dz-fabriquant/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sayara-dz-fabriquant/static/css/2.fc8123a0.chunk.css"
   },
   {
-    "revision": "a9c703444ad6e5111a8c",
+    "revision": "338567098fbe48e7d32c",
     "url": "/sayara-dz-fabriquant/static/css/main.60c276da.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sayara-dz-fabriquant/static/js/2.3236881e.chunk.js"
   },
   {
-    "revision": "a9c703444ad6e5111a8c",
-    "url": "/sayara-dz-fabriquant/static/js/main.68f429d4.chunk.js"
+    "revision": "338567098fbe48e7d32c",
+    "url": "/sayara-dz-fabriquant/static/js/main.deb1852c.chunk.js"
   },
   {
     "revision": "4ed25d9da19ee73831ab",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sayara-dz-fabriquant/precache-manifest.06c788fcd05f3a705879e3acf182bde4.js"
+  "/sayara-dz-fabriquant/precache-manifest.df693f98b87160af314744afee766fa5.js"
 );
 
 self.addEventListener('message', (event) => {
